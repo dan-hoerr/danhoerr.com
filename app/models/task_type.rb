de443e7c173bd96f18e02a882c09cd4796d6100f
Dan Hoerr <dhoerr@gmail.com>
2012-07-16 19:14:17 -0400

    removing deleted files... quite a buildup

--- app/models/task_type.rb
+++
@@ -1,4 +0,0 @@
-class TaskType < ActiveRecord::Base
-  attr_accessible :name, :typeclass
-  belongs_to :task
-end
--- test/unit/helpers/task_types_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class TaskTypesHelperTest < ActionView::TestCase
-end
