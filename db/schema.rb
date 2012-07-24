99a422c5a00cd7f7547314a5cbd408cf9afb2c57
Dan Hoerr <dhoerr@gmail.com>
2012-07-23 20:10:29 -0400

    added role to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120716224247) do
+ActiveRecord::Schema.define(:version => 20120724000457) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(:version => 20120716224247) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.integer  "role"
   end
 
 end
