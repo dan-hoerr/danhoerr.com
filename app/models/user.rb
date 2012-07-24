c70e0e6e036b2fde93e53093efa7d9f1f3a4a8ed
Dan Hoerr <dhoerr@gmail.com>
2012-07-24 18:52:40 -0400

    forgot to make role accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :role
   
   has_secure_password
 
